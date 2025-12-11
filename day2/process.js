da3b13cdb1733c2be797d6537d7bd52d6f945b24
gareth5james <gareth5james@yahoo.co.uk>
2025-12-11 16:47:40 +0000

    part 2

--- day2/process.js
+++ day2/process.js
@@ -18,4 +18,39 @@ for (id of input) {
   }
 }
 
-fs.writeFileSync("./output.txt", `Part 1: ${invalidIds.reduce((a, b) => a + b, 0).toString()}`);
+fs.writeFileSync(
+  "./output.txt",
+  `Part 1: ${invalidIds.reduce((a, b) => a + b, 0).toString()}`
+);
+
+//Part 2
+invalidIds = [];
+
+for (const range of input) {
+  const start = parseInt(range[0], 10);
+  const end = parseInt(range[1], 10);
+
+  for (let i = start; i <= end; i++) {
+    const numStr = i.toString();
+    const len = numStr.length;
+    let isRepeating = false;
+
+    for (let partLen = 1; partLen <= Math.floor(len / 2); partLen++) {
+      if (len % partLen !== 0) continue;
+
+      const part = numStr.slice(0, partLen);
+      const repeated = part.repeat(len / partLen);
+      if (repeated === numStr) {
+        isRepeating = true;
+        break;
+      }
+    }
+
+    if (isRepeating) invalidIds.push(i);
+  }
+}
+
+fs.appendFileSync(
+  "./output.txt",
+  ` Part 2: ${invalidIds.reduce((a, b) => a + b, 0).toString()}`
+);
