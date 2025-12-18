348c35f9e742dc96c573e99aa3835aaf848e312b
gareth5james <gareth5james@yahoo.co.uk>
2025-12-18 09:02:12 +0000

    day3 part 2

--- day3/process.js
+++ day3/process.js
@@ -6,7 +6,7 @@ const input = fs
   .split("\n")
   .map((line) => line.split("").map(Number));
 
-let count = 0;
+let total = 0;
 for (const line of input) {
   let max1 = 0;
   let max2 = 0;
@@ -17,7 +17,36 @@ for (const line of input) {
       max2 = 0;
     } else if (joltage > max2) max2 = joltage;
   }
-  count += max1 * 10 + max2;
+  total += max1 * 10 + max2;
 }
 
-fs.writeFileSync("./output.txt", `Part1: ${count.toString()}`);
+fs.writeFileSync("./output.txt", `Part 1: ${total.toString()}`);
+
+total = 0n;
+
+for (const line of input) {
+  const digits = line;
+  const toRemove = digits.length - 12;
+
+  const stack = [];
+  let removalsLeft = toRemove;
+
+  for (const d of digits) {
+    while (
+      stack.length > 0 &&
+      removalsLeft > 0 &&
+      stack[stack.length - 1] < d
+    ) {
+      stack.pop();
+      removalsLeft--;
+    }
+    stack.push(d);
+  }
+
+  const bestDigits = stack.slice(0, 12);
+  const value = BigInt(bestDigits.join(""));
+
+  total += value;
+}
+
+fs.appendFileSync("./output.txt", `\nPart 2: ${total.toString()}`);
