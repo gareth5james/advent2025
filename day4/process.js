5a2222a9b73eb0f39dd57ad125a1a507f519632e
gareth5james <gareth5james@yahoo.co.uk>
2025-12-18 11:59:19 +0000

    refactor

--- day4/process.js
+++ day4/process.js
@@ -6,74 +6,55 @@ const input = fs
   .split("\n")
   .map((line) => line.split(""));
 
-let rollCount = 0;
 const character = "@";
+
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
+function countNeighbors(grid, i, j, character, accessor = (v) => v) {
+  let count = 0;
+
+  for (const [di, dj] of directions) {
+    const ni = i + di;
+    const nj = j + dj;
+
+    if (
+      ni >= 0 &&
+      ni < grid.length &&
+      nj >= 0 &&
+      nj < grid[ni].length &&
+      accessor(grid[ni][nj]) === character
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+let rollCount = 0;
 
 for (let i = 0; i < input.length; i++) {
   for (let j = 0; j < input[i].length; j++) {
-    let count = 0;
     if (input[i][j] === character) {
-      if (i === 0 && j === 0) {
-        if (input[i + 1][j] === character) count++;
-        if (input[i][j + 1] === character) count++;
-        if (input[i + 1][j + 1] === character) count++;
-      } else if (i === 0 && j === input[i].length - 1) {
-        if (input[i + 1][j] === character) count++;
-        if (input[i][j - 1] === character) count++;
-        if (input[i + 1][j - 1] === character) count++;
-      } else if (i === input.length - 1 && j === 0) {
-        if (input[i - 1][j] === character) count++;
-        if (input[i][j + 1] === character) count++;
-        if (input[i - 1][j + 1] === character) count++;
-      } else if (i === input.length - 1 && j === input[i].length - 1) {
-        if (input[i - 1][j] === character) count++;
-        if (input[i][j - 1] === character) count++;
-        if (input[i - 1][j - 1] === character) count++;
-      } else if (i === 0) {
-        if (input[i][j - 1] === character) count++;
-        if (input[i][j + 1] === character) count++;
-        if (input[i + 1][j - 1] === character) count++;
-        if (input[i + 1][j] === character) count++;
-        if (input[i + 1][j + 1] === character) count++;
-      } else if (i === input.length - 1) {
-        if (input[i][j - 1] === character) count++;
-        if (input[i][j + 1] === character) count++;
-        if (input[i - 1][j - 1] === character) count++;
-        if (input[i - 1][j] === character) count++;
-        if (input[i - 1][j + 1] === character) count++;
-      } else if (j === 0) {
-        if (input[i - 1][j] === character) count++;
-        if (input[i + 1][j] === character) count++;
-        if (input[i - 1][j + 1] === character) count++;
-        if (input[i][j + 1] === character) count++;
-        if (input[i + 1][j + 1] === character) count++;
-      } else if (j === input[i].length - 1) {
-        if (input[i - 1][j] === character) count++;
-        if (input[i + 1][j] === character) count++;
-        if (input[i - 1][j - 1] === character) count++;
-        if (input[i][j - 1] === character) count++;
-        if (input[i + 1][j - 1] === character) count++;
-      } else {
-        if (input[i - 1][j - 1] === character) count++;
-        if (input[i - 1][j] === character) count++;
-        if (input[i - 1][j + 1] === character) count++;
-        if (input[i][j - 1] === character) count++;
-        if (input[i][j + 1] === character) count++;
-        if (input[i + 1][j - 1] === character) count++;
-        if (input[i + 1][j] === character) count++;
-        if (input[i + 1][j + 1] === character) count++;
-      }
-      if (count < 4) rollCount++;
+      const neighbors = countNeighbors(input, i, j, character);
+      if (neighbors < 4) rollCount++;
     }
   }
 }
 
 fs.writeFileSync("./output.txt", `Part 1: ${rollCount}`);
 
-let tempInput = input.map((line) =>
-  line.map((ch) => {
-    return { char: ch, move: false };
-  })
+let tempInput = input.map((row) =>
+  row.map((ch) => ({ char: ch, move: false }))
 );
 
 rollCount = 0;
@@ -81,75 +62,23 @@ let trueCount = 1;
 
 while (trueCount > 0) {
   trueCount = 0;
-  tempInput = tempInput.map((line) =>
-    line.map((obj) => {
-      if (obj.move) {
-        return { char: ".", move: false };
-      } else {
-        {
-          return obj;
-        }
-      }
-    })
+
+  tempInput = tempInput.map((row) =>
+    row.map((cell) => (cell.move ? { char: ".", move: false } : cell))
   );
+
   for (let i = 0; i < tempInput.length; i++) {
     for (let j = 0; j < tempInput[i].length; j++) {
-      let count = 0;
       if (tempInput[i][j].char === character) {
-        if (i === 0 && j === 0) {
-          if (tempInput[i + 1][j].char === character) count++;
-          if (tempInput[i][j + 1].char === character) count++;
-          if (tempInput[i + 1][j + 1].char === character) count++;
-        } else if (i === 0 && j === tempInput[i].length - 1) {
-          if (tempInput[i + 1][j].char === character) count++;
-          if (tempInput[i][j - 1].char === character) count++;
-          if (tempInput[i + 1][j - 1].char === character) count++;
-        } else if (i === tempInput.length - 1 && j === 0) {
-          if (tempInput[i - 1][j].char === character) count++;
-          if (tempInput[i][j + 1].char === character) count++;
-          if (tempInput[i - 1][j + 1].char === character) count++;
-        } else if (
-          i === tempInput.length - 1 &&
-          j === tempInput[i].length - 1
-        ) {
-          if (tempInput[i - 1][j].char === character) count++;
-          if (tempInput[i][j - 1].char === character) count++;
-          if (tempInput[i - 1][j - 1].char === character) count++;
-        } else if (i === 0) {
-          if (tempInput[i][j - 1].char === character) count++;
-          if (tempInput[i][j + 1].char === character) count++;
-          if (tempInput[i + 1][j - 1].char === character) count++;
-          if (tempInput[i + 1][j].char === character) count++;
-          if (tempInput[i + 1][j + 1].char === character) count++;
-        } else if (i === tempInput.length - 1) {
-          if (tempInput[i][j - 1].char === character) count++;
-          if (tempInput[i][j + 1].char === character) count++;
-          if (tempInput[i - 1][j - 1].char === character) count++;
-          if (tempInput[i - 1][j].char === character) count++;
-          if (tempInput[i - 1][j + 1].char === character) count++;
-        } else if (j === 0) {
-          if (tempInput[i - 1][j].char === character) count++;
-          if (tempInput[i + 1][j].char === character) count++;
-          if (tempInput[i - 1][j + 1].char === character) count++;
-          if (tempInput[i][j + 1].char === character) count++;
-          if (tempInput[i + 1][j + 1].char === character) count++;
-        } else if (j === tempInput[i].length - 1) {
-          if (tempInput[i - 1][j].char === character) count++;
-          if (tempInput[i + 1][j].char === character) count++;
-          if (tempInput[i - 1][j - 1].char === character) count++;
-          if (tempInput[i][j - 1].char === character) count++;
-          if (tempInput[i + 1][j - 1].char === character) count++;
-        } else {
-          if (tempInput[i - 1][j - 1].char === character) count++;
-          if (tempInput[i - 1][j].char === character) count++;
-          if (tempInput[i - 1][j + 1].char === character) count++;
-          if (tempInput[i][j - 1].char === character) count++;
-          if (tempInput[i][j + 1].char === character) count++;
-          if (tempInput[i + 1][j - 1].char === character) count++;
-          if (tempInput[i + 1][j].char === character) count++;
-          if (tempInput[i + 1][j + 1].char === character) count++;
-        }
-        if (count < 4) {
+        const neighbors = countNeighbors(
+          tempInput,
+          i,
+          j,
+          character,
+          (cell) => cell.char
+        );
+
+        if (neighbors < 4) {
           rollCount++;
           tempInput[i][j].move = true;
           trueCount++;
